cc126cfa302a089101f01feadf040d69ab82b423
Dan Qian <daniel.j.qian@gmail.com>
2021-03-28 14:58:24 -0400

    follow links instead of going to wikipedia

--- static/js/home.js
+++ static/js/home.js
@@ -1,11 +1,34 @@
 
+
+function handleWikipediaLink(e) 
+{
+    e.preventDefault();
+    const linkEl = e.currentTarget;
+    
+    // Disable the other links, otherwise we might load multiple links
+    document.querySelectorAll("#wikipedia-frame a").forEach((el) =>{
+        el.onclick = (e) => {
+            e.preventDefault();
+            console.log("prevent multiple click");
+        };
+    });
+
+    // Ignore external links
+    if (linkEl.getAttribute("rel") !== "mw:WikiLink") return;
+
+    loadPage(linkEl.getAttribute("href"))
+}
+
 async function loadPage(page) {
     const resp = await fetch(
         `https://en.wikipedia.org/api/rest_v1/page/html/${page}?redirect=false`,
     )
     const body = await resp.text()
-    console.log(body)
     document.getElementById("wikipedia-frame").innerHTML = body
+
+    document.querySelectorAll("#wikipedia-frame a").forEach((el) =>{
+        el.onclick = handleWikipediaLink;
+    });
 }
 
 function loadArticle() {
