463300d5be9c11fdd5a1d9484a3dd62755d8540c
Dan Qian <daniel.j.qian@gmail.com>
2021-03-29 18:22:18 -0400

    a little cleanup

--- app.py
+++ app.py
@@ -2,10 +2,6 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
-#@app.route('/', methods=['GET'])
-#def get_home():
-#    return render_template('home.html', name='home')
-
 @app.route('/', methods=['GET'])
 def get_start():
-    return render_template('start.html')#, start='United States', end='Gross domestic product')
+    return render_template('start.html')
