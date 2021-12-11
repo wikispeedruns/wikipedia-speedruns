811d79cb3c75b80b99eb6dec0fbccfcfe34b590f
mliu59 <61032765+mliu59@users.noreply.github.com>
2021-12-10 21:51:41 -0500

    added more guiding tips

--- static/js/play.js
+++ static/js/play.js
@@ -185,7 +185,21 @@ function displayTimer() {
 }
 
 function getRandTip() {
-    return "There are five permanent members of the UN security council: China, France, Russia, United Kingdom, and the United States."
+    tips = [
+        "The Fortune magazine has a list for top 500 United States companies (“Fortune 500”), as well as a list for top 500 global companies (“Fortune Global 500”).",
+        "Brazil is currently the world's largest producer of sugarcane, and by a lot!",
+        "Pressing the back button will forfeit your attempt!",
+        "Infoboxes on the right often give very quick and useful links, especially for biographical and geographical pages.",
+        "Plan ahead, but be flexible! If you foresee a better route than what you had planned, go for it!",
+        "Reminder: “Find on page” is against the rules!",
+        "There are five permanent members of the UN security council: China, France, Russia, United Kingdom, and the United States.",
+        "Use the Table of Contents to your advantage!",
+        "Buddhism originated in ancient India sometime between the 6th and 4th centuries BCE.",
+
+    ];
+
+    return tips[Math.floor(Math.random() * tips.length)];
+    
 }
 
 function countdownOnLoad(start, end) {
@@ -205,8 +219,13 @@ function countdownOnLoad(start, end) {
     timerBlock.style.display = "none";
     tipsBlock.style.display = "block";
 
+    var tipsHeader = document.createElement("p");
+    tipsHeader.innerHTML = "Here's a tip that may or may not be helpful!"
+    var tip = document.createElement("p");
+    tip.innerHTML = getRandTip();
 
-    tipsBlock.innerHTML = getRandTip();
+    tipsBlock.appendChild(tipsHeader);
+    tipsBlock.appendChild(tip);
 
     //countdownBlock.innerHTML = "Prompt will begin in " + "5" + " seconds";
     /*    <img class="startgun" src="{{url_for('static', filename='assets/startgun.gif')}}">
