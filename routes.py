199011f5cbdab29f3a5ca591c6beae2299c17dde
Dan Qian <daniel.j.qian@gmail.com>
2022-01-27 12:20:02 -0500

    fix

--- routes.py
+++ routes.py
@@ -8,10 +8,9 @@ import db
 import random
 
 # Passes session args to function if needed
-def render_with_user(template, **kwargs):
+def render_with_data(template, **kwargs):
 
     data = kwargs
-
 
     if ("user_id" in session):
         data["user_id"] = session["user_id"]
@@ -23,12 +22,12 @@ def render_with_user(template, **kwargs):
 
 # Front end pages
 @app.route('/', methods=['GET'])
-def get_home_page():
-    return render_with_user('home.html')
+def get_home_page():    
+    return render_with_data('home.html')
 
 @app.route('/about', methods=['GET'])
-def get_about_page():
-    return render_with_user('about.html')
+def get_about_page():    
+    return render_with_data('about.html')
 
 @app.route('/random', methods=['GET'])
 def get_random_prompt():
@@ -56,58 +55,55 @@ def get_latest_prompt():
 
 @app.route('/register', methods=['GET'])
 def get_register_page():
-    return render_with_user('users/register.html')
+    return render_with_data('users/register.html')
 
 
 @app.route('/pending', methods=['GET'])
 def get_create_oauth_account_page():
     if ("pending_oauth_creation" in session):
-        return render_with_user('users/pending.html')
+        return render_with_data('users/pending.html')
     else:
         return redirect('/')
 
 @app.route('/login', methods=['GET'])
 def get_login_page():
-    return render_with_user('users/login.html')
+    return render_with_data('users/login.html')
 
 @app.route('/profile/<username>', methods = ['GET'])
 def get_profile_page(username):
-    return render_with_user('profile.html', profile_name=username)
+    return render_with_data('profile.html', profile_name=username)
 
 @app.route('/manage', methods=['GET'])
 @check_admin
 def get_manage_page():
-    return render_with_user('manage.html')
+    return render_with_data('manage.html')
 
 @app.route('/prompt/<id>', methods=['GET'])
 def get_prompt_page(id):
     run_id = request.args.get('run_id', '')
 
     if len(run_id) != 0:
-        return render_with_user('prompt.html', prompt_id=id, run_id=run_id)
+        return render_with_data('prompt.html', prompt_id=id, run_id=run_id)
     else:
-        return render_with_user('prompt.html', prompt_id=id)
+        return render_with_data('prompt.html', prompt_id=id)
 
 
 @app.route('/play/<id>', methods=['GET'])
 def get_play_page(id):
-    return render_with_user('play.html', prompt_id=id)
-
-
+    return render_with_data('play.html', prompt_id=id)
 
 @app.route('/confirm/<token>', methods=['GET'])
 def get_confirm_page(token):
-    return render_with_user('users/confirm_email.html', token=token)
+    return render_with_data('users/confirm_email.html', token=token)
 
 @app.route('/reset/request', methods=['GET'])
 def get_reset_request_page():
-    return render_template('users/reset_password_request.html')
+    return render_with_data('users/reset_password_request.html')
 
 @app.route('/reset/<id>/<token>', methods=['GET'])
 def get_reset_page(id, token):
-    return render_template('users/reset_password.html', id=id, token=token)
-
+    return render_with_data('users/reset_password.html', id=id, token=token)
 
 @app.route('/error', methods=['GET'])
 def get_gen_error_page():
-    return render_with_user('users/generic_error.html')
+    return render_with_data('users/generic_error.html')
