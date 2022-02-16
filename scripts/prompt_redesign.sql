6c4a2affb1254f0e395e3ca7fad08456fbdf8e56
Daniel Qian <daniel.j.qian@gmail.com>
2022-02-16 00:54:02 -0500

    Fix prompt_redesign.sql

--- scripts/prompt_redesign.sql
+++ scripts/prompt_redesign.sql
@@ -1,9 +1,9 @@
 INSERT INTO `sprint_prompts` (`prompt_id`, `start`, `end`, `rated`, `active_start`, `active_end`)
-SELECT 
-	p.prompt_id, `start`, `end`, 
-    IF(d.rated IS NULL, 0, d.rated), 
-    IF(d.date IS NULL, CURDATE(), d.date), 
-    DATE_ADD(IF(d.date IS NULL, CURDATE() + 7, d.date + 1), INTERVAL -1 SECOND)
+SELECT
+    p.prompt_id, `start`, `end`,
+    IF(d.rated IS NULL, 0, d.rated),
+    IF(d.date IS NULL, DATE_ADD(CURDATE(), INTERVAL -10 DAY), d.date),
+    IF(d.date IS NULL, DATE_ADD(CURDATE(), INTERVAL -9 DAY),  DATE_ADD(d.date, INTERVAL 1 DAY))
 FROM prompts as p
 LEFT JOIN daily_prompts as d ON p.prompt_id = d.prompt_id;
 
