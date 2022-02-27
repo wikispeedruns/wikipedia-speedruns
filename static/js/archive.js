b3592dc79e03917ead6add453c40228f64b528fb
Arpan Sahoo <asahoojhu@gmail.com>
2022-02-27 06:26:45 -0500

    Display link to leaderboard in archive page

--- static/js/archive.js
+++ static/js/archive.js
@@ -14,10 +14,15 @@ var app = new Vue({
         numPages: 0,
 
         limit: 0,
-        offset: 0
+        offset: 0,
+        loggedIn: false,
     },
 
     created: async function() {
+        if ("username" in serverData) {
+            this.loggedIn = true;
+        }
+        
         const response = await fetch(`/api/sprints/archive?limit=${limit}&offset=${offset}`);
         const resp = await response.json();
 
