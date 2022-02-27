9598bfa42a3db4f3d6acc1aa3c0d4e4f6e57a06b
Nathan Vallapureddy <nvallap1@jhu.edu>
2022-02-27 13:01:57 -0500

    sort using the column header... but the url doesn't update

--- static/js/archive.js
+++ static/js/archive.js
@@ -14,9 +14,34 @@ var app = new Vue({
         page: 0,
         numPages: 0,
 
-        limit: 0,
+        limit: 20,
         offset: 0,
-        sort_desc: 0
+        sort_desc: sort_desc
+    },
+
+    methods : {
+        sortButton: function() {
+            if (this.sort_desc) {
+                return `<i class="bi bi-chevron-down"></i>`
+            } else {
+                return `<i class="bi bi-chevron-up"></i>`
+            }
+        },
+
+        toggleSort: async function() {
+            const dest = `?limit=${limit}&offset=0&sort_desc=${!this.sort_desc}`;
+            const response = await fetch("/api/sprints/archive" + dest);
+            const resp = await response.json();
+            this.prompts = resp['prompts'];
+            this.page = 1;
+            this.offset = 0;
+            this.sort_desc = !this.sort_desc;
+            console.log(this.sort_desc);
+        },
+
+        runReplay: function(event) {
+            console.log(event)
+        }
     },
 
     created: async function() {
