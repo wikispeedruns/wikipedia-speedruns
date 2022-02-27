36aca55e94e33d8458e7f9419e4cd57d70d0834c
Jason <codethejason@outlook.com>
2022-02-26 23:42:15 -0800

    merge

--- static/js/modules/game/filters.js
+++ static/js/modules/game/filters.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 /** 
  * File to define all the filters used in the game.
  */
@@ -6,6 +5,3 @@
 const pathArrowFilter = (arr) => arr.join(' → ');
 
 export {pathArrowFilter};
-=======
-export const pathArrowFilter = (arr) => arr.join(' → ');
->>>>>>> d9cc567ca2e458489a148a6ce0906c8e7b916f77
