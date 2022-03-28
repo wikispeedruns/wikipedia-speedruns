d1a17d139de7a1bd9a72e9ceb409b48ee26231ca
mliu59 <61032765+mliu59@users.noreply.github.com>
2022-03-27 23:55:48 -0400

    sprint mobile UI fixes (#263)
    * sprint mobile UI fix, redid timebox, reduced font size
    
    * added current article
    
    * mobile sprint timebox resize

--- static/js/modules/game/articleRenderer.js
+++ static/js/modules/game/articleRenderer.js
@@ -95,7 +95,7 @@ export class ArticleRenderer {
 
 function hideElements(frame) {
 
-    const hide = ["reference","mw-editsection","reflist","portal","refbegin", "sidebar", "authority-control", "external", "sistersitebox"]
+    const hide = ["reference","mw-editsection","reflist","portal","refbegin", "sidebar", "authority-control", "external", "sistersitebox", "box-More_citations_needed"]
     for(let i=0; i<hide.length; i++) {
         let elements = document.getElementsByClassName(hide[i])
         //console.log("found: " + hide[i] + elements.length)
