7e7fa7b3693098bcc8c003f3bf3b439c86505bc5
mliu59 <61032765+mliu59@users.noreply.github.com>
2022-06-15 19:40:43 -0400

    added achievements tab to user account page

--- static/js/account.js
+++ static/js/account.js
@@ -1,6 +1,7 @@
 import { serverData } from "./modules/serverData.js";
 import { fetchJson } from "./modules/fetch.js";
 import { profileStatsTable } from "./modules/profileStats.js";
+import { achievement_table } from "./modules/achievements.js";
 
 
 var app = new Vue({
@@ -9,6 +10,7 @@ var app = new Vue({
 
     components: {
         'profile-stats-table': profileStatsTable,
+        'achievement-table': achievement_table
     },
     
     data: {
@@ -18,6 +20,33 @@ var app = new Vue({
         feedbackMsg: '',
         confirmText: "Delete my account permanently!",
         tab:'',
+
+
+        list: achievement_list,
+
+        testData: {
+            "bathroom": {
+                "time_reached": "2009-06-15T13:45:30",
+                "achieved": 1
+            }, 
+            "usa": {
+                "time_reached": "2019-06-15T13:45:30",
+                "achieved": 0,
+                "reached": 7,
+                "out_of": 50
+            }, 
+            "sparta": {
+                "time_reached": "2009-06-15T13:45:30",
+                "achieved": 0
+            }, 
+            "fastest_gun_alive": {
+                "time_reached": "2019-06-15T13:45:30",
+                "achieved": 1
+            }, 
+            "meta": {
+                "time_reached": "2019-06-15T13:45:30",
+                "achieved": 1
+            }}
     },
 
     created: async function() {
