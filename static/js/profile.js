54fb5b7aec0153beb092ae5b031c2ab4674fe23d
mliu59 <61032765+mliu59@users.noreply.github.com>
2022-06-15 19:36:45 -0400

    added achievement rendering

--- static/js/profile.js
+++ static/js/profile.js
@@ -1,6 +1,37 @@
 import { profileStatsTable } from "./modules/profileStats.js";
+import { achievement_table } from "./modules/achievements.js";
 
 var app = new Vue({
+    delimiters: ['[[', ']]'],
     el: '#app',
-    components: {'profile-stats': profileStatsTable }
+    components: {'profile-stats': profileStatsTable, 'achievement-table': achievement_table },
+
+    data: {
+
+        list: achievement_list,
+
+        testData: {
+            "bathroom": {
+                "time_reached": "2009-06-15T13:45:30",
+                "achieved": 1
+            }, 
+            "usa": {
+                "time_reached": "2019-06-15T13:45:30",
+                "achieved": 0,
+                "reached": 7,
+                "out_of": 50
+            }, 
+            "sparta": {
+                "time_reached": "2009-06-15T13:45:30",
+                "achieved": 0
+            }, 
+            "fastest_gun_alive": {
+                "time_reached": "2019-06-15T13:45:30",
+                "achieved": 1
+            }, 
+            "meta": {
+                "time_reached": "2019-06-15T13:45:30",
+                "achieved": 1
+            }}
+    }
 });
