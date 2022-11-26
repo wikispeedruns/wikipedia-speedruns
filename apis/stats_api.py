d4f8df4c24fb3d2eeebf860d37c42e43852080a3
Brice Halder <bricehalder@meta.com>
2022-11-26 02:21:46 -0800

    better task queue

--- apis/stats_api.py
+++ apis/stats_api.py
@@ -9,10 +9,20 @@ from util.decorators import check_admin
 
 stats_api = Blueprint("stats", __name__, url_prefix="/api/stats")
 
+background_tasks = set()
+
 @stats_api.get("/calculate")
 @check_admin
 def calculate_stats():
-    asyncio.create_task(stats.async_calculate_stats())
+    if len(background_tasks) > 0:
+        return 'Task in progress', 400
+        
+    task = asyncio.create_task(stats.async_calculate_stats())
+    # strong reference so task doesn't get GC'd
+    background_tasks.add(task)
+
+    # clean up after task finishes
+    task.add_done_callback(background_tasks.discard)
     return 'Success', 200
 
 @stats_api.get("/all")
