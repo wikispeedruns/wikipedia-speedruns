f6dfe3620efeffd1471136233ca6ca9cd61bc10a
Wesley Lin <wesleylin625@gmail.com>
2023-11-19 09:56:17 -0800

    add nav js code and wiki css

--- static/js/pages/nav.js
+++ static/js/pages/nav.js
@@ -1,7 +1,55 @@
 
 import Vue from 'vue/dist/vue.esm.js';
 
+// getTheme retrieves theme from localStorage
+// returns string of either "dark" or "light"
+function getTheme() {
+    let currentTheme = localStorage.getItem("user-theme");
+    if (currentTheme == "dark") {
+        return "dark";
+    } else {
+        return "light";
+    }
+}
 
+async function setTheme(theme) {
+    var htmlElement = document.querySelector('html');
+
+    // set bootstrap theme to root html div
+    // https://getbootstrap.com/docs/5.3/customize/color-modes/
+    htmlElement.setAttribute('data-bs-theme', theme);
+
+    setWikiTheme(theme);
+}
+
+// theme:string is either 'dark' or 'light'
+async function setWikiTheme(theme) {
+    if (theme == "dark") {
+        var toc = document.querySelector('#toc');
+        if (toc != null) {
+            toc.classList.add("wiki-dark");
+        }
+
+        const allTables = document.querySelector('table');
+        if (allTables != null) {
+            allTables.classList.add("wiki-dark");
+        }
+    } else {
+        var toc = document.querySelector('#toc');
+        if (toc != null) {
+            toc.classList.remove("wiki-dark");
+        }
+
+        const allTables = document.querySelector('table');
+        if (allTables != null) {
+            allTables.classList.remove("wiki-dark");
+        }
+    }
+}
+
+function saveTheme(theme) {
+
+}
 
 var app = new Vue({
     delimiters: ['[[', ']]'],
@@ -24,6 +72,11 @@ var app = new Vue({
         async accountPage(event)
         {
             window.location.href = "/account"
+        },
+        async switchTheme() {
+            let currentTheme = getTheme();
+
+            
         }
     }
 
