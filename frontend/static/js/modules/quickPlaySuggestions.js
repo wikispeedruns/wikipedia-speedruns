8078604ebd5adc1168e0a3b7fd555ada53b735c4
mliu59 <61032765+mliu59@users.noreply.github.com>
2024-01-27 17:25:52 -0500

    removed h6 tag

--- frontend/static/js/modules/quickPlaySuggestions.js
+++ frontend/static/js/modules/quickPlaySuggestions.js
@@ -24,7 +24,7 @@ var quickPlaySuggestions = {
 	template: (`
         <div>
             <div class="container" style="width: 100%;">
-                <h6>Here's what other players have been playing recently: </h6>
+                <div>Here's what other players have been playing recently: </div>
                 <div class="row" v-for='p in suggested'>
                     <div class="col">
                         {{p.prompt_start}} <span><i class="bi bi-arrow-right-short"></i></span> {{p.prompt_end}}
