a44e40360d2382fecece5513226cdeb238de8f76
scooterbreak <jasonjuwonkim@gmail.com>
2024-09-02 21:17:50 -0700

    fixing get_embeddings.sh script

--- get_embeddings.sh
+++ get_embeddings.sh
@@ -1,7 +1,7 @@
 #/bin/bash
 
 EMBEDDINGS_FILE="data/wiki2vec.txt.bz2"
-if [[ -f $EMBEDDINGS_FILE ]]; then
+if [[ ! -f $EMBEDDINGS_FILE ]]; then
     mkdir -p data
     wget "http://wikipedia2vec.s3.amazonaws.com/models/en/2018-04-20/enwiki_20180420_100d.txt.bz2" -O $EMBEDDINGS_FILE.bz2
     bunzip2 $EMBEDDINGS_FILE.bz2
